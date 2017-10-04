415d498186a6b4042b64524864f3ab05fce5a0b6
amberjohnsonsmile <31632938+amberjohnsonsmile@users.noreply.github.com>
2017-10-03 20:20:56 -0600

    Uploaded JS file

---
+++ new-insults.js
@@ -0,0 +1,8 @@
+var bodyParts = ["fingernail", "split end", "kneecap"];
+var adjectives = ["smarmy", "part-time-job-having", "Facebook-stalking"];
+var words = ["defunct starship", "pig", "hangnail", "barf puddle", "floof"];
+var bodyPart = bodyParts[Math.floor(Math.random() * bodyParts.length)];
+var adjective = adjectives[Math.floor(Math.random() * adjectives.length)];
+var word = words[Math.floor(Math.random() * words.length)];
+var randomInsult = "You have a " + bodyPart + " that reminds me of a " + adjective + " " + word + "!!";
+randomInsult;
