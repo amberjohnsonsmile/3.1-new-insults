0e897fc24689df051c47a579c774f301480865ff
amberjohnsonsmile <31632938+amberjohnsonsmile@users.noreply.github.com>
2017-10-04 09:31:06 -0600

    Challenge #4 - joining numbers

---
+++ 4 joining-numbers.js
@@ -0,0 +1,5 @@
+//Challenge: take the array [3, 2, 1] and express is as "3 is bigger than 2 is bigger than 1" using join
+
+var numbers = [3, 2, 1];
+var numbersJoin = numbers.join(" is bigger than ");
+numbersJoin;
