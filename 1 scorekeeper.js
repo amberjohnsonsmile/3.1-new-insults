14450210010d19f68d73cb7fa0275567870e93cc
amberjohnsonsmile <31632938+amberjohnsonsmile@users.noreply.github.com>
2017-10-04 12:26:49 -0600

    Ch 4 Challenge #1 - Scorekeeper

---
+++ 1 scorekeeper.js
@@ -0,0 +1,23 @@
+// Challenge - design a scores object with the ability to add points
+
+var scores = {
+  stella: 0,
+  sahan: 0,
+  sam: 0,
+  madison: 0
+};
+
+//Add 2 points for Madison
+scores.madison += 2;
+scores.madison;
+
+//Add 2 points for Sam
+scores.sam += 2;
+scores.sam;
+
+//Subtract 1 point for Madison
+scores.madison -= 1;
+scores.madison;
+
+//List all scores
+scores;
