9c00b0c2848f8df38acbcffc99889c908e7205d6
amberjohnsonsmile <31632938+amberjohnsonsmile@users.noreply.github.com>
2017-10-04 09:23:30 -0600

    Challenge #2 - Use + or Join

---
+++ 3 use-add-or-join.js
@@ -0,0 +1,18 @@
+// Challenge: create a random insult generator, then experiment with putting the insult together using both + and .join(" ")
+
+// First section is used for both
+var relatives = ["mother", "father", "sister", "brother"];
+var adjectives = ["smelly", "fart-tastic", "rancid", "corpulent", "barnacle-eating"];
+var occupations = ["shepherd", "doorman", "plunderer", "substitute teacher", "financial consultant", "hobbit hunter"];
+
+var relative = relatives[Math.floor(Math.random() * relatives.length)];
+var adjective = adjectives[Math.floor(Math.random() * adjectives.length)];
+var occupation = occupations[Math.floor(Math.random() * occupations.length)];
+
+// Putting the insult together using +
+andInsult = "Your " + relative + "'s greatest ambition was to become a " + adjective + " " + occupation + "!!";
+andInsult;
+
+//Putting the insult together using .join(" ")
+joinInsult = ["Your", relative + "'s greatest ambition was to become a", adjective, occupation + "!!"].join(" ");
+joinInsult;
