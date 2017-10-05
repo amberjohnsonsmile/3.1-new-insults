054d3fb9de86df95c24ed08ef8d546d3cb5f5cc0
amberjohnsonsmile <31632938+amberjohnsonsmile@users.noreply.github.com>
2017-10-05 10:46:07 -0600

    Update 6.2random-string-generator.js

--- 6.2random-string-generator.js
+++ 6.2random-string-generator.js
@@ -1,9 +1,9 @@
-// Use a loop with letters of the alphabet to generate a string of random letters
+// Use a while loop with letters of the alphabet to generate a string of random letters
 
 var alphabet = "abcdefghijklmnopqrstuvwxyz";
 var randomString = "";
 
-for (i = 0; i < 6; i++) {
+while (randomString.length < 6) {
   randomString += alphabet[Math.floor(Math.random() * alphabet.length)];
 }
 randomString;
