3fe205792c2dd1be0a88652a6d526532aa93810c
amberjohnsonsmile <31632938+amberjohnsonsmile@users.noreply.github.com>
2017-10-06 12:21:39 -0600

    Add files via upload

---
+++ 8.1doing-arithmetic-with-functions.js
@@ -0,0 +1,11 @@
+// Challenge: create add and multiply functions
+
+function add(number1, number2) {
+  return number1 + number2;
+}
+
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
+add(multiply(36325, 9824), 777);
