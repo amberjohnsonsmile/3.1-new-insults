80659080138d96c0b085d1083a6fb5fce7543119
amberjohnsonsmile <31632938+amberjohnsonsmile@users.noreply.github.com>
2017-10-06 12:52:42 -0600

    Add files via upload

---
+++ 8.2are-these-arrays-the-same.js
@@ -0,0 +1,13 @@
+// Challenge: create a function to tell if two arrays contain the same numbers in the same order
+
+function areArraysSame(array1, array2) {
+  // Exit the function if the arrays are different lengths
+  if (array1.length !== array2.length) {
+    return;
+  }
+
+  // Compare the arrays
+  for (i = 0; i < array1.length; i++) {
+    return array1[i] === array2[i];
+  }
+}
